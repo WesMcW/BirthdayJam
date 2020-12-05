51fbb77b765e2d8d5e41591437850d8c78b3f8ee
Leah Blasczyk <BlasczykLeah@users.noreply.github.com>
2020-12-05 15:31:48 -0600

    new button test

---
+++ VrJam/Assets/Scripts/ButtonLogic.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonLogic : MonoBehaviour
+{
+    public GameObject myButton;
+    public bool pushed = false;
+
+    float yPos = -1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!pushed && other.gameObject == myButton)
+        {
+            pushed = true;
+            ButtonPressed();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (pushed && other.gameObject == myButton)
+        {
+            pushed = false;
+            ButtonReleased();
+        }
+    }
+
+    protected virtual void ButtonPressed()
+    {
+        Debug.Log("button pressed!");
+    }
+
+    protected virtual void ButtonReleased()
+    {
+        Debug.Log("button released!");
+    }
+
+    private void Start()
+    {
+        yPos = myButton.transform.localPosition.y;
+    }
+
+    private void Update()
+    {
+        if(myButton.transform.localPosition.y < 0)
+        {
+            //reset
+            myButton.transform.localPosition = new Vector3(0, yPos, 0);
+        }
+    }
+}
---
+++ VrJam/Assets/Scripts/testbutton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testbutton : ButtonLogic
+{
+    public Material newMat;
+    public Material oldMat;
+
+    protected override void ButtonPressed()
+    {
+        base.ButtonPressed();
+        myButton.GetComponent<MeshRenderer>().material = newMat;
+    }
+
+    protected override void ButtonReleased()
+    {
+        base.ButtonReleased();
+        myButton.GetComponent<MeshRenderer>().material = newMat;
+    }
+}
