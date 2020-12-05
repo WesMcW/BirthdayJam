dfa4076808153b7ec75c4358395a27b22b9df7ca
Leah Blasczyk <BlasczykLeah@users.noreply.github.com>
2020-12-05 11:37:43 -0600

    started product stuffs

---
+++ VrJam/Assets/Scripts/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Product : MonoBehaviour
+{
+    public ProductPool myPool;
+    public ProductType myType;
+
+    public void Return()
+    {
+        //returns to location of myReset and gets added back to its pool
+        myPool.Return(gameObject);
+    }
+}
+
+public enum ProductType
+{
+    thing1,
+    thing2,
+    thing3
+}
---
+++ VrJam/Assets/Scripts/ProductPool.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductPool : MonoBehaviour
+{
+    GameObject myObject;
+
+    public void Return(GameObject obj)
+    {
+        if (!myObject) myObject = obj;
+        myObject.SetActive(true);
+        myObject.transform.position = transform.position;
+    }
+}
