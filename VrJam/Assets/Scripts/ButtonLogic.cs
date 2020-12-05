724195be7dab67fa2d2ea1fa1f9d7d909cb85550
Leah Blasczyk <BlasczykLeah@users.noreply.github.com>
2020-12-05 15:57:52 -0600

    button?

--- VrJam/Assets/Scripts/ButtonLogic.cs
+++ VrJam/Assets/Scripts/ButtonLogic.cs
@@ -7,29 +7,45 @@ public class ButtonLogic : MonoBehaviour
     public GameObject myButton;
     public bool pushed = false;
 
+    protected MeshRenderer myRender;
+    Material defaultMat;
+    public Material pushedMat;
+
     float yPos = -1;
 
     private void OnTriggerEnter(Collider other)
     {
-        if(!pushed && other.gameObject == myButton)
+        //if(!pushed && other.gameObject == myButton)
+        if(!pushed && other.CompareTag("Hand"))
         {
             pushed = true;
+            myRender.material = pushedMat;
             ButtonPressed();
+            StartCoroutine(DelayReset());
         }
     }
 
-    private void OnTriggerExit(Collider other)
+    /*private void OnTriggerExit(Collider other)
     {
-        if (pushed && other.gameObject == myButton)
+        //if (pushed && other.gameObject == myButton)
+        if (pushed && other.CompareTag("Hand"))
         {
             pushed = false;
             ButtonReleased();
         }
-    }
+    }*/
 
     protected virtual void ButtonPressed()
     {
         Debug.Log("button pressed!");
+    }
+
+    IEnumerator DelayReset()
+    {
+        yield return new WaitForSeconds(3F);
+        myRender.material = defaultMat;
+        pushed = false;
+        ButtonReleased();
     }
 
     protected virtual void ButtonReleased()
@@ -40,6 +56,8 @@ public class ButtonLogic : MonoBehaviour
     private void Start()
     {
         yPos = myButton.transform.localPosition.y;
+        myRender = GetComponent<MeshRenderer>();
+        defaultMat = myRender.material;
     }
 
     private void Update()
